f75efe6cdd6e901a6c1cc59a93e23e95e57ce1e4
Jefanya Efandchris <53685179+jefanya14@users.noreply.github.com>
2022-10-07 16:43:44 +0700

    Update get_flag.php

--- system/get_flag.php
+++ system/get_flag.php
@@ -1,32 +1,44 @@
 <?php
 
-
-function get_client_ip() {
-    $ipaddress = '';
-    if (getenv('HTTP_CLIENT_IP'))
-        $ipaddress = getenv('HTTP_CLIENT_IP');
-    else if(getenv('HTTP_X_FORWARDED_FOR'))
-        $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
-    else if(getenv('HTTP_X_FORWARDED'))
-        $ipaddress = getenv('HTTP_X_FORWARDED');
-    else if(getenv('HTTP_FORWARDED_FOR'))
-        $ipaddress = getenv('HTTP_FORWARDED_FOR');
-    else if(getenv('HTTP_FORWARDED'))
-       $ipaddress = getenv('HTTP_FORWARDED');
-    else if(getenv('REMOTE_ADDR'))
-        $ipaddress = getenv('REMOTE_ADDR');
-    else
-        $ipaddress = 'IP tidak dikenali';
-    return $ipaddress;
+// MENDAPATKAN ALAMAT IP PRIBADI SI TARGET
+function getClientIP() {
+$ipaddress = '';
+if (getenv('HTTP_CLIENT_IP'))
+$ipaddress = getenv('HTTP_CLIENT_IP');
+else if(getenv('HTTP_X_FORWARDED_FOR'))
+$ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+else if(getenv('HTTP_X_FORWARDED'))
+$ipaddress = getenv('HTTP_X_FORWARDED');
+else if(getenv('HTTP_FORWARDED_FOR'))
+$ipaddress = getenv('HTTP_FORWARDED_FOR');
+else if(getenv('HTTP_FORWARDED'))
+$ipaddress = getenv('HTTP_FORWARDED');
+else if(getenv('REMOTE_ADDR'))
+$ipaddress = getenv('REMOTE_ADDR');
+else
+$ipaddress = 'UNKNOWN';
+return $ipaddress;
 }
-$ip = "".get_client_ip()."";
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL,"https://arpanrizki.my.id/api/get_flag2/api.php"); 
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS,"ip=$ip");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$jefanya_flag = curl_exec($ch);
-curl_close ($ch);
-$exe = explode('-',$jefanya_flag);
-$hasil_jefanya_flag = $exe[1];
+
+// MENGIRIM ALAMAT IP PRIBADI SI TARGET KE SERVER UNTUK DILACAK
+$url = "https://jefanya.website/api/geolocation/?ip=".getClientIP();
+
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$resp = curl_exec($curl);
+		curl_close($curl);
+$data = json_decode($resp,true);
+
+// HASIL PELACAKAN ALAMAT IP PRIBADI SI TARGET
+$arpantek_ip_address = getClientIP();
+$arpantek_flag = $data['flag'];
+$arpantek_callingcode = $data['code'];
+$arpantek_continent = $data['continent'];
+$arpantek_country = $data['country'];
+$arpantek_region = $data['region'];
+$arpantek_city = $data['city'];
+$arpantek_latitude = $data['latitude'];
+$arpantek_longitude = $data['longitude'];
 ?>
